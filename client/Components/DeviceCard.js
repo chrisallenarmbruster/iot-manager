fbd6d45acb3d979927be9c97f7c65ae7cef5fa47
Chris Armbruster <chris@armbrustermail.com>
2024-02-09 08:53:34 -0600

    add and integrate "last-event" route for devices

--- client/Components/DeviceCard.js
+++ client/Components/DeviceCard.js
@@ -1,55 +1,75 @@
-import React from "react"
-import { connect } from "react-redux"
-import Card from "react-bootstrap/Card"
-import Spinner from "react-bootstrap/Spinner"
-import { Link } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import Card from "react-bootstrap/Card";
+import Spinner from "react-bootstrap/Spinner";
+import { Link } from "react-router-dom";
+import axios from "axios"; // Make sure to install axios if not already installed
 
 const DeviceCard = (props) => {
-  const deviceId = props.deviceId
+  const [lastEvent, setLastEvent] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchLastEvent = async () => {
+      try {
+        const response = await axios.get(
+          `/api/devices/${props.device.id}/last-event`
+        );
+        console.log("last event response.data", response.data);
+        setLastEvent(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching last event:", error);
+        setIsLoading(false);
+      }
+    };
+
+    if (props.deviceCurrentValues.length === 0) {
+      fetchLastEvent();
+    }
+  }, [props.deviceId, props.deviceCurrentValues.length]);
+
   return (
-    <Link to={`/devices/${props.device.id}`} n className="text-decoration-none">
+    <Link to={`/devices/${props.device.id}`} className="text-decoration-none">
       <Card
-        className="h-100\ bg-info text-dark p-2"
-        title={`${props.device.model} | MAC: ${props.device.mac} ${
-          props.deviceCurrentValues.length > 0
-            ? `| IP: ${props.deviceCurrentValues[0]?.ip}`
-            : ""
-        }`}
+        className="h-100 bg-info text-dark p-2"
+        title={`${props.device.model} | MAC: ${props.device.mac}`}
       >
         <Card.Title className="fw-bold">
           {props.device.deviceconfig.label}
         </Card.Title>
-        {/* <Card.Subtitle className="text-light">Temperature</Card.Subtitle> */}
         <Card.Body
           className="text-center display-1 fw-bold my-3"
           style={{ minHeight: "125px" }}
         >
-          {props.deviceCurrentValues.length > 0 ? (
-            <React.Fragment>
-              <div>{props.deviceCurrentValues[0]?.value.slice(0, -1)}&deg;</div>
-            </React.Fragment>
-          ) : (
+          {props.deviceCurrentValues.length > 0 &&
+          typeof props.deviceCurrentValues[0]?.value === "string" ? (
+            <div>{props.deviceCurrentValues[0].value.slice(0, -1)}&deg;</div>
+          ) : isLoading ? (
             <div className="d-flex justify-content-center">
               <Spinner animation="grow" role="status" className="mt-4">
                 <span className="visually-hidden">Loading...</span>
               </Spinner>
             </div>
+          ) : (
+            lastEvent &&
+            typeof lastEvent.value === "string" && (
+              <div>{lastEvent.value.slice(0, -1)}&deg;</div>
+            )
           )}
         </Card.Body>
       </Card>
     </Link>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    device: state.deviceAll.data.find(
-      (device) => device.id === ownProps.deviceId
-    ),
-    deviceCurrentValues: state.deviceCurrentValues.data.filter(
-      (deviceCurrentValue) => deviceCurrentValue.deviceId === ownProps.deviceId
-    ),
-  }
-}
+const mapStateToProps = (state, ownProps) => ({
+  device: state.deviceAll.data.find(
+    (device) => device.id === ownProps.deviceId
+  ),
+  deviceCurrentValues: state.deviceCurrentValues.data.filter(
+    (deviceCurrentValue) => deviceCurrentValue.deviceId === ownProps.deviceId
+  ),
+});
 
-export default connect(mapStateToProps)(DeviceCard)
+export default connect(mapStateToProps)(DeviceCard);
--- server/api.js
+++ server/api.js
@@ -1,19 +1,41 @@
-const express = require("express")
-const app = express.Router()
-const Event = require("./db/event")
-const Device = require("./db/device")
-const DeviceConfig = require("./db/deviceconfig")
+const express = require("express");
+const app = express.Router();
+const Event = require("./db/event");
+const Device = require("./db/device");
+const DeviceConfig = require("./db/deviceconfig");
 
-module.exports = app
+module.exports = app;
 
 app.get("/devices", async (req, res, next) => {
   try {
-    const devices = await Device.findAll({ include: [{ model: DeviceConfig }] })
-    res.send(devices)
+    const devices = await Device.findAll({
+      include: [{ model: DeviceConfig }],
+    });
+    res.send(devices);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+app.get("/devices/:id/last-event", async (req, res, next) => {
+  console.log("req.params.id", req.params.id);
+  try {
+    const lastEvent = await Event.findOne({
+      where: { deviceId: req.params.id },
+      order: [["time", "DESC"]],
+      limit: 1,
+    });
+
+    if (lastEvent) {
+      console.log("lastEvent", lastEvent);
+      res.send(lastEvent);
+    } else {
+      res.status(404).send({ message: "No events found for this device." });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.get("/devices/:id", async (req, res, next) => {
   try {
@@ -23,7 +45,7 @@ app.get("/devices/:id", async (req, res, next) => {
           model: DeviceConfig,
         },
       ],
-    })
+    });
     const events = await Event.findAll({
       where: { deviceId: req.params.id },
       attributes: [
@@ -37,12 +59,12 @@ app.get("/devices/:id", async (req, res, next) => {
       ],
       order: [["time", "DESC"]],
       limit: 240,
-    })
-    res.send({ ...device.dataValues, events })
+    });
+    res.send({ ...device.dataValues, events });
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 app.get("/events", async (req, res, next) => {
   try {
@@ -60,9 +82,9 @@ app.get("/events", async (req, res, next) => {
       include: [{ model: Device, include: [{ model: DeviceConfig }] }],
       order: [["time", "DESC"]],
       limit: 240,
-    })
-    res.send(events)
+    });
+    res.send(events);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
